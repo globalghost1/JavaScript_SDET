42f3aab30cbc6271cf73091b5bf4526e1e594769
Patryk <globalghost1@protonmail.com>
2023-07-29 19:52:16 +0200

    BasicsJS2

--- JavaScript Programming basics from Scratch/basics1.js
+++ JavaScript Programming basics from Scratch/basics1.js
@@ -2,14 +2,32 @@ console.log("Hello World")
 
 
 let a=4
+console.log(a)
+console.log(typeof(a))
 
-let b= 234.6
+let b=234.6
+console.log(typeof(b))
+
+var c = "Patryk Ksiazek"
+console.log(typeof(c))
+
+let required = true
+console.log(typeof(required))
+//null and undefined 
+
+
+console.log(typeof(required))
+//null und undefined
+// let c = a+b (it did not work //we cannot redeclare variable with let keyword bur possible)
+var c = a+b// reassining allowed with let
+console.log(c)  //we cannot redeclare variable with let keyword but possible with war
+console.log(!required)
+
 
 
 
 
 //var , (let, const) ES6 
-
 //these are comments
 /*
 comment
---
+++ JavaScript Programming basics from Scratch/basics2.js
@@ -0,0 +1,41 @@
+const flag = true 
+
+if(flag)
+{
+console.log("condition satisfied")
+}
+else
+{
+console.log("condition not satisfied")
+}
+let i  = 0 
+while(i<10)
+{
+i++
+console.log(i)
+}
+
+do 
+{
+i++
+}while(i>10);
+console.log(i)
+
+for(let k=0;k<=10;k++)
+{
+    console.log(k)
+}
+
+let required = true
+while(required)
+{
+console.log(required)
+required= false
+}
+
+
+
+
+
+
+
