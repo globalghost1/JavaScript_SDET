b53fb3f7236262b075479c29f98810e94f2be55d
Patryk <globalghost1@protonmail.com>
2023-07-31 20:00:30 +0200

    Arrays_and_Methods_in_JS

--- JavaScript Programming basics from Scratch/basics2.js
+++ JavaScript Programming basics from Scratch/basics2.js
@@ -47,4 +47,3 @@ required= false
 
 
 
-
---
+++ JavaScript Programming basics from Scratch/basics3.js
@@ -0,0 +1,21 @@
+let marks = Array(6)
+var marks = new Array(20,40,35,12,37,100)
+
+var marks = [20,40,35,12,37,100]
+console.log (marks[2]) //35
+marks[3] = 14
+console.log(marks) //[20,40,35,14,37,100]
+console.log(marks.length) //6 
+marks.push(65)
+console.log(marks) // [20,40,35,14,37,100]
+marks.pop()//[20,40,35,14,37,100]
+marks.unshift(12)
+console.log(marks)
+
+
+
+
+
+
+
+
