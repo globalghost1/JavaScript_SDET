0466ec4a5b4e65f05468ff179a26d290e893b3a6
Patryk <globalghost1@protonmail.com>
2023-08-13 19:26:17 +0200

    JS_String_and_its_manipulation_methods

---
+++ JavaScript Programming basics from Scratch/basics5.js
@@ -0,0 +1,16 @@
+let day = 'tuesday'
+console.log(day.length)  //8
+let subDay = day.slice(0,5)
+console.log(subDay)
+console.logh(day[1]) //u
+//tue day
+let splitDay = day.split("s")
+console.log(splitDay[1].length)
+console.log(splitDay[1].trim().length)
+
+let date = '23'
+let nextDate = '27'
+let diff = parseInt(nextDate) - parseInt(date)
+console.log(diff)
+diff.toString()
+
